554509780833395fc1bf453f5dd77d115a3f07a0
20240411김미래브랜치 경로 바꿔서 올린거임 <>
2024-04-16 00:28:37 +0900

    sts

--- niceweb/src/main/java/com/dima/niceweb/exchangeRate/ExchangeRateController.java
+++ niceweb/src/main/java/com/dima/niceweb/exchangeRate/ExchangeRateController.java
@@ -9,8 +9,7 @@ public class ExchangeRateController {
 
 	@GetMapping("/rate")
 	public String rate() {
-		//Map<String, Double> result =rateService.restTest();
-		//log.info("결과값 출력: "+ result);
+		
 		return "Main/exchangeRate";
 	}
 
