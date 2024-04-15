23234d99a2f75b08e5c2e226a4b04e9c26a160db
20240411김미래브랜치 경로 바꿔서 올린거임 <>
2024-04-15 20:59:39 +0900

    안에 있는 파일 모두 삭제하고 다시 올림

---
+++ niceweb/src/main/java/com/dima/niceweb/globe/GlobeController.java
@@ -0,0 +1,13 @@
+package com.dima.niceweb.globe;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GlobeController {
+    @GetMapping("/globeindex")
+    public String globeindex() {
+
+        return "globeindex";
+    }
+}
