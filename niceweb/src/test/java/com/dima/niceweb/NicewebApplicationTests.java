30b7273e1b12192080e702447b0ccf53a40b1bcb
20240411김미래브랜치 경로 바꿔서 올린거임 <>
2024-04-15 23:44:57 +0900

    환율 rate 부분 수정하고 컨트롤러 생성

---
+++ niceweb/src/test/java/com/dima/niceweb/NicewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dima.niceweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
