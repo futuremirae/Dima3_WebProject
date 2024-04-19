fb19880e807d7b80dea928c61af34c1ed0d7d089
김미래 <>
2024-04-19 18:51:25 +0900

    미래 이메일 부분 수정

--- niceweb/src/main/java/com/dima/niceweb/email/EmailService.java
+++ niceweb/src/main/java/com/dima/niceweb/email/EmailService.java
@@ -10,36 +10,34 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
-@RequiredArgsConstructor 
+@RequiredArgsConstructor
 @Slf4j
 public class EmailService {
 	private JavaMailSender mailSender;
-	//private static final String From_ADDRESS= "mirae1296@gmail.com";
+	// private static final String From_ADDRESS= "mirae1296@gmail.com";
 
-//	 public MailService(JavaMailSender mailSender) {
-//	        this.mailSender = mailSender;
-//	    }
-	
-	 @Autowired
-	    public EmailService(JavaMailSender mailSender) {
-	        this.mailSender = mailSender;
-	    }
-	
-	 @Value("${spring.mail.username}")
-	 String username;
+	// public MailService(JavaMailSender mailSender) {
+	// this.mailSender = mailSender;
+	// }
+
+	@Autowired
+	public EmailService(JavaMailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+
+	@Value("${spring.mail.username}")
+	String username;
+
 	public void mailSend(EmailDTO mailDTO) {
-		log.info("하ㅓㅓㅜㄹㄴ알눌노ㅓㅏ~~~~~~~~~~"+username);
-		
-		
+		log.info("하ㅓㅓㅜㄹㄴ알눌노ㅓㅏ~~~~~~~~~~" + username);
+
 		SimpleMailMessage message = new SimpleMailMessage();
 		message.setTo(mailDTO.getAddress());
 		message.setFrom(username);
-        message.setSubject(mailDTO.getTitle());
-        message.setText(mailDTO.getMessage());
-        log.info("###########"+message.toString());
-        mailSender.send(message);
-		
-	
+		message.setSubject(mailDTO.getTitle());
+		message.setText(mailDTO.getMessage());
+		log.info("###########" + message.toString());
+		mailSender.send(message);
 
-} 
 	}
+}
