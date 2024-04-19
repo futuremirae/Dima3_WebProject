cd5bf411ca00780a2491286d3f12bb25d84ea92a
김미래 <>
2024-04-19 14:50:49 +0900

    깃 이그노어 파일 수정함

--- niceweb/src/main/java/com/dima/niceweb/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dima.niceweb.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-	UserEntity findByUserId(String userId);
-
-}
